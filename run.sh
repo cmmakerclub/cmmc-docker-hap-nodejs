948b47915de66d4e187e23ee096e301f8e25c869
Nat <nat@cmmc.io>
2016-11-23 11:24:50 +0700

    update script

--- run.sh
+++ run.sh
@@ -6,6 +6,15 @@ sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
 dbus-daemon --system
 avahi-daemon -D 
 
+if [ -f /root/.initialized ];
+then
+   echo "starting.."
+else
+   touch /root/.initialized
+   /root/npm-install
+fi
+
 cd /root/HAP-NodeJS/
 DEBUG=* node BridgedCore.js
+
 

fb1bfa721456430a484159b253518691497fe966
Nat <nat@cmmc.io>
2016-11-23 10:57:59 +0700

    Update script
    Signed-off-by: Nat <nat@cmmc.io>

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-cd /root/HAP-NodeJS
-rm -rvf /root/HAP-NodeJS/node_modules
-rm -rvf /root/HAP-NodeJS/persist
-npm install
-
--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@ rm /var/run/dbus/pid
 sed -i "s/rlimit-nproc=3/#rlimit-nproc=3/" /etc/avahi/avahi-daemon.conf
 
 dbus-daemon --system
-avahi-daemon -D
+avahi-daemon -D 
 
-
-ifconfig
 cd /root/HAP-NodeJS/
 DEBUG=* node BridgedCore.js
 

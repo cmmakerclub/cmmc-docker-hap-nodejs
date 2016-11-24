122abdd1a8c8800f892fdf2599a01b82148f86c6
nazt-cmmc-io <nat@cmmc.io>
2016-11-25 03:17:58 +0700

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,1 +1,1 @@
-docker run  -it --net=host -v $(pwd)/HAP-NodeJS:/root/HAP-NodeJS cmmc/hap-nodejs:rpi
+docker run --name pisiriok -d  -it --net=host -v $(pwd)/HAP-NodeJS:/root/HAP-NodeJS cmmc/hap-nodejs:rpi
